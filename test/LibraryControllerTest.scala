1c6eca13c8ee1611363da4f8448056ebcf2ee447
Radu <cricanr@gmail.com>
2015-10-13 21:36:12 +0200

    Fixed 1st n2n test

--- test/LibraryControllerTest.scala
+++ test/LibraryControllerTest.scala
@@ -1,14 +1,9 @@
 package com.ebook.library.controllers
 
-import org.scalatest.mock.MockitoSugar
-import org.scalatestplus.play.{PlaySpec, OneAppPerTest}
+import org.scalatestplus.play.{PlaySpec}
 import play.api.http.{HttpProtocol, Status, HeaderNames}
 import play.api.test._
-import scala.concurrent.Future
-
 import org.scalatestplus.play.OneAppPerTest
-
-import scala.concurrent.Future
 
 class PlaySpecWrapper
   extends PlaySpec
@@ -21,13 +16,14 @@ class PlaySpecWrapper
   with Writeables
   with EssentialActionCaller
   with RouteInvokers
-//  with FutureAwaits
+  with FutureAwaits
 
 class LibraryControllerTest  extends PlaySpecWrapper with OneAppPerTest  {
 
   "GET ebook library should" in {
     val Some(response) = route(FakeRequest(GET, "/ebook-library/books"))
     status(response) mustEqual OK
-    response.toString must be("""{welcome to ebook library!}""")
+    contentAsString(response) must be("""{welcome to ebook library!}""")
   }
-  }
+
+}
